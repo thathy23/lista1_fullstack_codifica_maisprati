3cf1eab5aabf4545ee355d02e6f8c9e30b38e3ff
Tatiane <thathy23@gmail.com>
2025-05-04 17:01:12 -0300

    exercício 3

--- exercicio3.js
+++ exercicio3.js
@@ -1,4 +1,16 @@
 // 3. Implemente um programa que recebe uma nota de 0 a 10 e classifica como
 // "Aprovado", "Recuperação", ou "Reprovado" utilizando if-else if.
 
+const prompt = require('prompt-sync') ()
+
+const nota = prompt('Digite sua nota: ')
+    if(nota >= 7 && nota <=10){
+        console.log('Aprovado')
+    }else if(nota < 7 && nota > 0){
+        console.log('Recuperação')
+    }else if(nota == 0){
+        console.log('Reprovado')        
+    }else{
+        console.log('Digite uma nota válida entre 0 e 10')
+    }
 
