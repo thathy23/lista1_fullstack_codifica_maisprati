ee17ea7346542cb1386521823b4510dd4b32f40c
Tatiane <thathy23@gmail.com>
2025-05-04 19:40:48 -0300

    exercicio 5

--- exercicios-js/exercicio5.js
+++ exercicios-js/exercicio5.js
@@ -1,6 +1,27 @@
 //5. Escreva um programa que calcula o Índice de Massa Corporal (IMC) de uma pessoa e
 // determina a categoria de peso (baixo peso, peso normal, sobrepeso, obesidade)
 // utilizando if-else.
+const prompt = require('prompt-sync')();
+
+let peso = parseFloat(prompt('Digite seu peso: '))
+let altura = parseFloat(prompt('Digite sua altura: '))
+
+let imc = peso / (altura*altura)
+
+console.log(`Seu IMC é: ${imc.toFixed(1)}`)
+
+
+if (imc < 18.5) {
+    console.log('Voce está com Baixo peso');
+  } else if (imc >= 18.5 && imc < 24.9) {
+    console.log('Voce está com Peso normal');
+  } else if (imc >= 25 && imc < 29.9) {
+    console.log('Voce está com Sobrepeso');
+  } else {
+    console.log('Voce está com Obesidade');
+  }
+
+
 
 
 
