c6afa4d3291ece641226dfde42673740e112491f
Tatiane Fontes <106564636+thathy23@users.noreply.github.com>
2025-05-13 12:00:56 -0300

    Update exercicio15.js

--- exercicio15.js
+++ exercicio15.js
@@ -1,1 +1,39 @@
+const prompt = require('prompt-sync')({ sigint: true });
 
+let continuar;
+
+do {
+    const numero = parseInt(prompt("Digite um número para verificar se pertence à sequência de Fibonacci: "));
+
+    let a = 0;
+    let b = 1;
+    let valorFibonacci = 0;
+
+    while (valorFibonacci < numero) {
+        valorFibonacci = a + b;
+        a = b;
+        b = valorFibonacci;
+    }
+
+    if (numero === 0 || valorFibonacci === numero) {
+        console.log(`O número ${numero} pertence à sequência de Fibonacci.`);
+    } else {
+        console.log(`O número ${numero} não pertence à sequência de Fibonacci.`);
+    }
+
+    do {
+        continuar = prompt("Deseja verificar outro número? (S/N): ").toUpperCase();
+
+        if (continuar !== "S" && continuar !== "N") {
+            console.log("Resposta inválida, digite 'S' para sim ou 'N' para não.");
+        }
+    } while (continuar !== "S" && continuar !== "N");
+
+} while (continuar === "S");
+
+console.log("Programa encerrado.");
+
+        } while (continuar.equals("S"));
+
+        console.log("Programa encerrado.");
+        sc.close();
